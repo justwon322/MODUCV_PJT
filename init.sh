60ee177305fd67bcbb957c709322609ce9ded0d7
James Yu <justwon322@gmail.com>
2022-10-19 17:18:05 +0900

    모두의 CV 프로젝트1

---
+++ init.sh
@@ -0,0 +1,7 @@
+conda create -n MODU_CV python=3.8
+activate MODU_CV 
+pip install tensorflow-macos
+pip install numpy
+pip install matplotlib
+pip install tensorflow_datasets
+pip install tensorflow-metal
